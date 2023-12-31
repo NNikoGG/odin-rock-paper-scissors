3ff52e96bec32d9afe0cf8722b0da311f8e1a55f
NNikoGG <geekgaming222@gmail.com>
2023-12-31 13:13:52 +0530

    Added a function to generate a random choice by the computer

--- script/script.js
+++ script/script.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+function getComputerChoice(){
+    let options = ["Rock", "Paper", "Scissors"];
+    let size = options.length;
+    let choice = Math.random() * (size);
+    choice = parseInt(choice);
+    if (choice==0){
+        return options[0];
+    }
+    else if (choice==1){
+        return options[1];
+    }
+    else{
+        return options[2];
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "Rock";
+  const computerSelection = getComputerChoice();
+
